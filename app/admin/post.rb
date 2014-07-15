68b7c4cc117fd0f72c20c2e19cff668cbed451c1
jum-s <vincent.jumeaux@gmail.com>
2014-07-15 13:38:13 +0200

    colors old_press.ttf

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,17 +1,11 @@
 ActiveAdmin.register Post do
+  permit_params :title, :body
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  permit_params do
-    permitted = [:title, :body]
-  #  permitted << :other if resource.something?
-  #  permitted
+  form do |texte|
+    texte.inputs "Post" do
+      texte.input :title
+      texte.input :body, as: :html_editor
+    end
+    texte.actions
   end
-  
 end
