98239aa01a0a1cbf68f388bf2aa95576d175f808
jum-s <vincent.jumeaux@gmail.com>
2014-07-16 12:41:57 +0200

    videos migration + photo front better

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,9 +1,17 @@
 ActiveAdmin.register Post do
-  permit_params :title, :body
+  permit_params :title, :body, :video
 
+  index do
+    selectable_column
+    column :title
+    column :updated_at
+    actions
+  end
+  
   form do |texte|
     texte.inputs "Post" do
       texte.input :title
+      texte.input :video
       texte.input :body, as: :html_editor
     end
     texte.actions
